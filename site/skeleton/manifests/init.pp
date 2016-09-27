4837bb4d17466bf9674df6eee53b3e6eceb7bda6
Joshua Paulik <joshua.paulik@noaa.gov>
2016-09-27 15:26:27 -0400

    skeleton class

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,16 @@
+#skeleton class
+
+class skeleton {
+
+file {'/etc/skel':
+  ensure => directory,
+  }
+  
+file {'/etc/skel/.bashrc':
+  ensure => present;
+  source => 'http:///modules/skeleton/bashrc
+  }
+
+
+}
+
