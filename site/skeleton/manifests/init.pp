0906b12ae337a5a4dcb22da1aa85524699bcf34a
Joshua Paulik <joshua.paulik@noaa.gov>
2016-09-27 15:32:31 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -11,7 +11,7 @@ file {'/etc/skel/.bashrc':
   owner => 'root',
   group => 'root',
   mode => '0644',
-  source => 'http:///modules/skeleton/bashrc',
+  source => 'puppet:///modules/skeleton/bashrc',
   }
 
 
