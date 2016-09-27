af0b95b7641faae7d04edfabdc8417103a8dda34
Joshua Paulik <joshua.paulik@noaa.gov>
2016-09-27 16:34:04 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,21 @@
 class memcached {
+
+package {'memcached':
+  ensure => present,
+  before => File['/etc/sysconfig/memcached'],
+         }
+        
+file {'/etc/sysconfig/memcached':
+  ensure => file,
+  owner => root,
+  source => 'puppet:///modules/memcached/memcached',
+  notify => Service['memcached'],    
+      }
+      
+service {'memcached':
+  ensure => running,
+  enable => true,
+  }
+
+
+}
