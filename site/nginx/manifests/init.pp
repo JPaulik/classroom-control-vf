81b7c5d4553d6fbebbcdaf8977b9bfd245c925a1
Joshua Paulik <joshua.paulik@noaa.gov>
2016-09-28 09:03:49 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -9,20 +9,42 @@ package {'nginx':
   
 file {'/etc/nginx/nginx.conf':
   ensure => present,
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
   source => 'puppet:///modules/nginx/nginx.conf',
   notify => Service['nginx'],
   }
  
 file {'/var/www':
   ensure => directory,
+  owner => root,
+  group => root,
   }
 
 file {'/var/www/index.html':
   ensure => present,
-  mode => '0777',
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
   source => 'puppet:///modules/nginx/index.html',
   }
+  
+file { '/etc/nginx/conf.d':
+  ensure => directory,
+  owner => 'root',
+  group => 'root',
+  mode => '0775',
+  }
 
+ file { '/etc/nginx/conf.d/default.conf':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
+  source => 'puppet:///modules/nginx/default.conf',
+  }
+  
 service {'nginx':
   ensure => running,
   enable => true,
