ff34debe52b67a6276aa981a8cbd316d48d2e230
Joshua Paulik <joshua.paulik@noaa.gov>
2016-09-29 10:39:52 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,27 @@
-
+class nginx::params {
+  
+  case $::osfamily {
+  'redhat','debian': {
+    $package = 'nginx'
+    $owner = 'root'
+    $group = 'root'
+    $docroot = '/var/www'
+    $configdir = '/etc/nginx'
+    $logdir = '/var/log/nginx'
+    }
+    'windows': {
+     $package = 'nginx-service'
+    $owner = 'Administrator'
+    $group = 'Administrators'
+    $docroot = 'C:/ProgramData/nginx/html'
+    $configdir = 'C:/ProgramData/nginx'
+    $logdir = 'C:/ProgramData/nginx/logs'
+    }
+  }
+  
+  $user=$::osfamily ?{
+  'redhat' => 'nginx',
+  'debian' => 'www-data',
+  'windows' => 'nobody',
+  }
+  }
