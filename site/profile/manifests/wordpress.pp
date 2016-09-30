980d4ed76de5778f361dc2ad63add589fe833040
Joshua Paulik <joshua.paulik@noaa.gov>
2016-09-29 20:57:47 -0400

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -10,7 +10,7 @@ class profile::wordpress {
     wp_owner    => 'wordpress',
   wp_group    => 'wordpress',
   db_user     => 'wordpress',
-  db_password => 'asdfasdf,
+  db_password => 'asdfasdf',
   }
     
   include wrappers::apache
